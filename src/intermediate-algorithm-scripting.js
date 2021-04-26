a505fff2f7ae9b17895a169ae8721bee8b533b38
Antonio Pierro <antonio.pierro@gmail.com>
2021-04-26 12:03:23 +0200

    add diffArray

--- src/intermediate-algorithm-scripting.js
+++ src/intermediate-algorithm-scripting.js
@@ -2,3 +2,6 @@ exports.sumAll = (arr) => {
     const [min, max] = [Math.min(...arr), Math.max(...arr)];
     return [...Array(max - min + 1)].reduce((a, c, i) => a + i + min, 0);
 };
+
+exports.diffArray = (arr1, arr2) =>
+    arr1.concat(arr2).filter((e) => !arr1.includes(e) || !arr2.includes(e));
--- tests/intermediate-algorithm-scripting.test.js
+++ tests/intermediate-algorithm-scripting.test.js
@@ -1,8 +1,22 @@
-const { sumAll } = require("../src/intermediate-algorithm-scripting");
+const {
+    sumAll,
+    diffArray,
+} = require("../src/intermediate-algorithm-scripting");
 
-test("Sum All Numbers in a RangePassed.", () => {
+test("Sum All Numbers in a RangePassed", () => {
     // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range
     expect(sumAll([1, 4])).toBe(10);
     expect(sumAll([5, 10])).toBe(45);
     expect(sumAll([10, 5])).toBe(45);
+});
+
+test("Diff Two ArraysPassed", () => {
+    // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/sum-all-numbers-in-a-range
+    expect(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])).toMatchObject([4]);
+    expect(
+        diffArray(
+            ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+            ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+        )
+    ).toMatchObject(["pink wool"]);
 });
