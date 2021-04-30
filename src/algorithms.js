2669234b73214bb6c448440bc12b9925494a2f32
Antonio Pierro <antonio.pierro@gmail.com>
2021-04-30 23:36:44 +0200

    feat: add Inventory Update solution

--- src/algorithms.js
+++ src/algorithms.js
@@ -3,3 +3,12 @@ exports.sym = (...args) => {
     const res = args.reduce((a, c) => a.concat(c).filter(onFilter(a, c)));
     return [...new Set(res)].sort();
 };
+
+exports.updateInventory = (arr1, arr2) => {
+    const arr2obj = (a, c) => ({ ...a, [c[1]]: c[0] });
+    const keys = [...new Set([...arr1, ...arr2].map((e) => e[1]))].sort();
+    const [obj1, obj2] = [arr1, arr2].map((e) => e.reduce(arr2obj, {}));
+    const sum = (a, c) => [...a, { [c]: (obj1[c] || 0) + (obj2[c] || 0) }];
+    const sumObj = keys.reduce(sum, []);
+    return sumObj.map((e) => Object.entries(e)[0].reverse());
+};
